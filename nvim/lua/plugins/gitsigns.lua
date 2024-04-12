f33b0cb372c28863df7270a73b793272fc3a6626
Nikos Batsaras <nickbatsaras@gmail.com>
2024-04-12 16:23:42 +0300

    Add new Gitsigns keymaps

--- nvim/lua/plugins/gitsigns.lua
+++ nvim/lua/plugins/gitsigns.lua
@@ -19,7 +19,7 @@ return {
                         },
                         auto_attach                       = true,
                         attach_to_untracked               = false,
-                        current_line_blame                = true, -- Toggle with `:Gitsigns toggle_current_line_blame`
+                        current_line_blame                = false, -- Toggle with `:Gitsigns toggle_current_line_blame`
                         current_line_blame_opts           = {
                                 virt_text = true,
                                 virt_text_pos = 'eol', -- 'eol' | 'overlay' | 'right_align'
@@ -43,6 +43,48 @@ return {
                                 row = 0,
                                 col = 1
                         },
+
+                        on_attach                         = function(bufnr)
+                                local gitsigns = require('gitsigns')
+
+                                local function map(mode, l, r, opts)
+                                        opts = opts or {}
+                                        opts.buffer = bufnr
+                                        vim.keymap.set(mode, l, r, opts)
+                                end
+
+                                -- Navigation
+                                map('n', ']c', function()
+                                        if vim.wo.diff then
+                                                vim.cmd.normal({ ']c', bang = true })
+                                        else
+                                                gitsigns.nav_hunk('next')
+                                        end
+                                end)
+
+                                map('n', '[c', function()
+                                        if vim.wo.diff then
+                                                vim.cmd.normal({ '[c', bang = true })
+                                        else
+                                                gitsigns.nav_hunk('prev')
+                                        end
+                                end)
+
+                                -- Actions
+                                map('n', '<leader>hs', gitsigns.stage_hunk)
+                                map('v', '<leader>hs',
+                                        function() gitsigns.stage_hunk { vim.fn.line('.'), vim.fn.line('v') } end)
+                                map('v', '<leader>hr',
+                                        function() gitsigns.reset_hunk { vim.fn.line('.'), vim.fn.line('v') } end)
+                                map('n', '<leader>hS', gitsigns.stage_buffer)
+                                map('n', '<leader>hu', gitsigns.undo_stage_hunk)
+                                map('n', '<leader>hR', gitsigns.reset_buffer)
+                                map('n', '<leader>hp', gitsigns.preview_hunk)
+                                map('n', '<leader>hb', function() gitsigns.blame_line { full = true } end)
+                                map('n', '<leader>tb', gitsigns.toggle_current_line_blame)
+                                map('n', '<leader>hd', gitsigns.diffthis)
+                                map('n', '<leader>hD', function() gitsigns.diffthis('~') end)
+                        end
                 }
         end
 }
