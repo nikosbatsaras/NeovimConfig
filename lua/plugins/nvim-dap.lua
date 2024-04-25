e9ec3768eabc242731dd37f6b8927b75c418ae0b
Nikos Batsaras <nikos.batsaras@futurae.com>
2024-04-25 16:41:16 +0300

    Configure nvim-dap for remote debugging
    Essentially this commit enables us to debug an application running
    inside the debugger in a Docker container. We can set breakpoints in the
    code and step through execution like normal.

--- lua/plugins/nvim-dap.lua
+++ lua/plugins/nvim-dap.lua
@@ -19,6 +19,8 @@ return {
                 vim.keymap.set('n', '<Leader>du', function() dap.step_out() end)
                 vim.keymap.set('n', '<Leader>db', function() dap.toggle_breakpoint() end)
 
+                vim.keymap.set('n', '<Leader>dt', function() dapgo.debug_test() end)
+
                 dap.listeners.before.attach.dapui_config = function()
                         dapui.open()
                 end
@@ -31,5 +33,28 @@ return {
                 dap.listeners.before.event_exited.dapui_config = function()
                         dapui.close()
                 end
+
+                dap.adapters.go_remote = {
+                        type = 'server',
+                        host = '127.0.0.1',
+                        port = 40000,
+                }
+
+                table.insert(dap.configurations.go, {
+                        type = 'go_remote',
+                        request = 'attach',
+                        name = 'Attach remote',
+                        mode = 'remote',
+                        substitutePath = { {
+                                from = "${workspaceFolder}",
+                                to = "/webapp/"
+                        } },
+                        connect = function()
+                                local host = vim.fn.input('Host [127.0.0.1]: ')
+                                host = host ~= '' and host or '127.0.0.1'
+                                local port = tonumber(vim.fn.input('Port [40000]: ')) or 40000
+                                return { host = host, port = port }
+                        end,
+                })
         end,
 }
