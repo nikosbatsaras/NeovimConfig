7c0a7efe9f7e30b7271999c2a6e2d4b245c9dc45
Nikos Batsaras <nikos.batsaras@futurae.com>
2024-04-26 21:05:42 +0300

    Migrate Vimscript function to RunTest() to Lua

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -48,25 +48,21 @@ keymap.set("n", "<F7>", "<cmd>setlocal spell! spelllang=el<CR>")
 keymap.set("i", "<C-a>", "<C-g>u<Esc>[s1z=`]a<C-g>u")
 keymap.set("n", "<C-a>", "i<C-g>u<Esc>[s1z=`]a<C-g>u<Esc>")
 
-vim.api.nvim_exec([[
-" Run Golang test function
-nnoremap <leader>t :call RunTest()<CR>
-
-function! RunTest()
-	let testName = expand("<cword>")
-	let currDir = expand("%:p:h:.")
-	let cmd = 'cd ' . currDir . ' && go test -testify.m ' . testName . ' ' . currDir
-	let output = system(cmd)
-	let lines = split(output, '\n')
-	execute 'new'
-	for line in lines
-		call append('$', line)
-	endfor
-	setlocal buftype=nofile bufhidden=wipe nobuflisted
-	setlocal readonly
-	execute 'resize 10'
-endfunction
-]], false)
+keymap.set("n", "<leader>T", function()
+        local testName = vim.fn.expand("<cword>")
+        local currDir = vim.fn.expand("%:p:h")
+        local cmd = 'cd ' .. currDir .. ' && go test -testify.m ' .. testName .. ' ' .. currDir
+        local output = vim.fn.system(cmd)
+        local lines = vim.fn.split(output, '\n')
+        vim.cmd('new')
+        for _, line in ipairs(lines) do
+                vim.fn.append('$', line)
+        end
+        vim.opt_local.buftype = 'nofile'
+        vim.opt_local.bufhidden = 'wipe'
+        vim.opt_local.readonly = true
+        vim.cmd('resize 10')
+end)
 
 vim.api.nvim_create_autocmd("BufWritePre", {
         buffer = buffer,
