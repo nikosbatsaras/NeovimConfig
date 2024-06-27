d8ff1cb6b0f0ea552dbc5f838659e60050aff8ec
Nikos Batsaras <nikos.batsaras@futurae.com>
2024-06-27 16:52:03 +0300

    Better organise Go imports
    Now we also remove unused imports on save.

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -65,8 +65,19 @@ keymap.set("n", "<leader>T", function()
 end)
 
 vim.api.nvim_create_autocmd("BufWritePre", {
-        buffer = buffer,
+        pattern = { "*.go" },
         callback = function()
-                vim.lsp.buf.format { async = false }
-        end
+                local params = vim.lsp.util.make_range_params(nil, "utf-16")
+                params.context = { only = { "source.organizeImports" } }
+                local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params, 3000)
+                for _, res in pairs(result or {}) do
+                        for _, r in pairs(res.result or {}) do
+                                if r.edit then
+                                        vim.lsp.util.apply_workspace_edit(r.edit, "utf-16")
+                                else
+                                        vim.lsp.buf.execute_command(r.command)
+                                end
+                        end
+                end
+        end,
 })
