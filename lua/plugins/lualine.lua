4aac8f054e1096fb806e8b3f6c2b36be19c3891b
Nikos Batsaras <nickbatsaras@gmail.com>
2025-10-25 14:49:16 +0300

    Update Lualine theme

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -43,7 +43,7 @@ return {
                                 -- Disable sections and component separators
                                 component_separators = '',
                                 section_separators = '',
-                                theme = "cyberdream",
+                                theme = "tokyonight-night",
                                 -- theme = {
                                 --         -- We are going to use lualine_c an lualine_x as left and
                                 --         -- right section. Both are highlighted by c theme .  So we
